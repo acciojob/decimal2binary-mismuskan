dd9fe9cd45cd97827537d208e650cee0ab1dd08a
Muskan Sheikh <144351188+mismuskan@users.noreply.github.com>
2024-01-09 19:05:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 function decimalToBinary(num) {
   //Write you code here
-  
+  let bin="";
+	while (num>0) {
+	let rem=num%2
+		bin=rem+bin;
+		 num = Math.floor(num / 2);
+	}
+	console.log(bin);
 }
 
 window.decimalToBinary = decimalToBinary;
